be50a03572739bf0fd9e4379650ac7defb3585b0
Aadi <aadijain1903@gmail.com>
2026-02-02 00:45:20 +0530

    make changes in main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,8 +10,12 @@ terraform {
 }
 
 provider "aws" {
-  region = var.aws_region
+  region                      = var.aws_region
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
 }
+
 
 # Conceptual app server
 resource "aws_instance" "cloudcostlens_app" {
